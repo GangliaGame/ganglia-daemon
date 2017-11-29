323ca533156460b9987ed7bfe2c35ad5cd584314
Avi Romanoff <avi@romanoff.me>
2017-11-29 01:55:40 -0500

    rpio hello world

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
-console.log('hello!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rpio = require("rpio");
+rpio.open(11, rpio.INPUT);
+console.log('Pin 11 is currently set ' + (rpio.read(11) ? 'high' : 'low'));
+// console.log('hello!')
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hello!')
+import * as rpio from 'rpio'
+
+rpio.open(11, rpio.INPUT);
+console.log('Pin 11 is currently set ' + (rpio.read(11) ? 'high' : 'low'));
+
+// console.log('hello!')
