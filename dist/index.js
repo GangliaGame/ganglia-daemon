4fc59e3a68454d1841d213115dbce4739abb9e98
Avi Romanoff <avi@romanoff.me>
2017-11-29 22:32:07 -0500

    progress

--- dist/index.js
+++ dist/index.js
@@ -5,12 +5,23 @@ const _ = require("lodash");
 const colors = require("colors/safe");
 const client_1 = require("./client");
 const panels_1 = require("./panels");
-const POLL_MSEC = 250;
+const WIRE_POLL_MSEC = 250;
+const BUTTON_POLL_MSEC = 50;
 const wires = {
     red: 3,
     blue: 5,
     yellow: 7,
 };
+const buttons = [
+    {
+        name: 'fire',
+        pin: 8,
+        toData: cur => cur,
+    }
+];
+function isButtonPressed(button) {
+    return Boolean(rpio.read(button.pin));
+}
 function panelWireIsPluggedInto(pin) {
     // Set all wire pins to LOW
     Object.values(wires).forEach(p => rpio.write(p, rpio.LOW));
@@ -44,19 +55,23 @@ function colorsForPanel(connections, panel) {
 (function main() {
     const serverUrl = process.env.GANGLIA_SERVER_URL || 'http://localhost:9000';
     const client = new client_1.Client(serverUrl);
-    // Set up color wires for writing
+    // Set up wire pins for writing
     Object.values(wires).forEach(pin => {
         rpio.open(pin, rpio.OUTPUT, rpio.LOW);
         rpio.pud(pin, rpio.PULL_DOWN);
     });
-    // Set up all pins for reading
+    // Set up all panel pins for reading
     _.flatten(_.map(panels_1.panels, 'pins')).forEach(pin => {
         rpio.open(pin, rpio.INPUT);
         rpio.pud(pin, rpio.PULL_DOWN);
     });
+    // Set up button pins for reading
+    buttons.forEach(({ pin }) => {
+        rpio.open(pin, rpio.INPUT, rpio.PULL_UP);
+    });
     // Periodically check for new connections
     let prevConnections = getConnections();
-    function poll() {
+    function pollWires() {
         const connections = getConnections();
         const newConnections = _.differenceWith(connections, prevConnections, _.isEqual);
         // If there were no new connections, just return early
@@ -77,21 +92,22 @@ function colorsForPanel(connections, panel) {
         events.map(event => client.emit(event));
         prevConnections = connections;
     }
-    // Begin polling
-    setInterval(poll, POLL_MSEC);
-    const pinA = 8;
-    rpio.open(8, rpio.INPUT, rpio.PULL_UP);
-    let A = 0;
-    function checkButton() {
-        const newA = rpio.read(pinA);
-        if (newA === A)
-            return;
-        console.log(`A = ${A}`);
-        A = newA;
+    let prev = false;
+    function pollButtons() {
+        buttons.map(button => {
+            const cur = isButtonPressed(button);
+            if (prev == cur)
+                return;
+            console.log(`${button.name} => ${cur ? 'pressed' : 'released'}}`);
+        });
     }
-    setInterval(checkButton, 50);
+    // Begin polling for wire connections
+    setInterval(pollWires, WIRE_POLL_MSEC);
+    // Begin polling for button presses
+    setInterval(pollButtons, BUTTON_POLL_MSEC);
     console.log('Ganglia Daemon is reborn!\n');
-    console.log(`${colors.bold('Poll rate')}: ${1000 / POLL_MSEC} Hz`);
+    console.log(`${colors.bold('Wire poll rate')}: ${1000 / WIRE_POLL_MSEC} Hz`);
+    console.log(`${colors.bold('Button poll rate')}: ${1000 / BUTTON_POLL_MSEC} Hz`);
     console.log(`${colors.bold('Server')}: ${serverUrl}`);
 })();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import * as rpio from 'rpio'
 import * as _ from 'lodash'
 import * as colors from 'colors/safe'
-import {WirePin, WireColor, Wire, Panel, Connection, Event} from './types'
+import {WirePin, WireColor, Wire, Pin, Panel, Connection, Event} from './types'
 import { Client } from './client'
 import { panels } from './panels'
-const POLL_MSEC = 250
+
+const WIRE_POLL_MSEC = 250
+const BUTTON_POLL_MSEC = 50
 
 const wires: Wire = {
   red: 3,
   blue: 5,
   yellow: 7,
+}
+
+type Button = {
+  name: string
+  pin: Pin
+  toData: (cur: boolean, prev: boolean) => any
+}
+
+const buttons: Array<Button> = [
+  {
+    name: 'fire',
+    pin: 8,
+    toData: cur => cur,
+  }
+]
+
+function isButtonPressed(button: Button): boolean {
+  return Boolean(rpio.read(button.pin))
 }
 
 function panelWireIsPluggedInto(pin: WirePin): Panel | null {
@@ -50,21 +70,26 @@ function colorsForPanel(connections: Array<Connection>, panel: Panel | null): Ar
   const serverUrl = process.env.GANGLIA_SERVER_URL || 'http://localhost:9000'
   const client = new Client(serverUrl)
 
-  // Set up color wires for writing
+  // Set up wire pins for writing
   Object.values(wires).forEach(pin => {
     rpio.open(pin, rpio.OUTPUT, rpio.LOW)
     rpio.pud(pin, rpio.PULL_DOWN);
-  });
+  })
 
-  // Set up all pins for reading
+  // Set up all panel pins for reading
   _.flatten(_.map(panels, 'pins')).forEach(pin => {
     rpio.open(pin, rpio.INPUT)
     rpio.pud(pin, rpio.PULL_DOWN);
-  });
+  })
+
+  // Set up button pins for reading
+  buttons.forEach(({pin}) => {
+    rpio.open(pin, rpio.INPUT, rpio.PULL_UP)
+  })
 
   // Periodically check for new connections
   let prevConnections: Array<Connection> = getConnections()
-  function poll() {
+  function pollWires() {
     const connections = getConnections()
     const newConnections: Array<Connection> = _.differenceWith(connections, prevConnections, _.isEqual)
 
@@ -89,23 +114,23 @@ function colorsForPanel(connections: Array<Connection>, panel: Panel | null): Ar
     prevConnections = connections
   }
 
-  // Begin polling
-  setInterval(poll, POLL_MSEC)
-
-  const pinA = 8
-  rpio.open(8, rpio.INPUT, rpio.PULL_UP);
-
-  let A = 0
-  function checkButton() {
-    const newA = rpio.read(pinA)
-    if (newA === A) return
-    console.log(`A = ${A}`)
-    A = newA
+  let prev = false
+  function pollButtons() {
+    buttons.map(button => {
+      const cur = isButtonPressed(button)
+      if (prev == cur) return
+      console.log(`${button.name} => ${cur ? 'pressed' : 'released'}}`)
+    })
   }
 
-  setInterval(checkButton, 50)
+  // Begin polling for wire connections
+  setInterval(pollWires, WIRE_POLL_MSEC)
+
+  // Begin polling for button presses
+  setInterval(pollButtons, BUTTON_POLL_MSEC)
 
   console.log('Ganglia Daemon is reborn!\n')
-  console.log(`${colors.bold('Poll rate')}: ${1000 / POLL_MSEC} Hz`)
+  console.log(`${colors.bold('Wire poll rate')}: ${1000 / WIRE_POLL_MSEC} Hz`)
+  console.log(`${colors.bold('Button poll rate')}: ${1000 / BUTTON_POLL_MSEC} Hz`)
   console.log(`${colors.bold('Server')}: ${serverUrl}`)
 })()
