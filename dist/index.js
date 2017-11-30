83826452ccf4a5e148172a60bda54ed32a78a178
Avi Romanoff <avi@romanoff.me>
2017-11-29 21:46:15 -0500

    progress

--- dist/index.js
+++ dist/index.js
@@ -87,7 +87,7 @@ function colorsForPanel(connections, panel) {
         var state = rpio.read(pin) ? 'pressed' : 'released';
         console.log('Button event on P%d (button currently %s)', pin, state);
     }
-    rpio.poll(8, pollcb, rpio.PULL_DOWN);
+    rpio.poll(8, pollcb, rpio.POLL_LOW);
     // rpio.poll(10, pollcb);
     console.log('Ganglia Daemon is reborn!\n');
     console.log(`${colors.bold('Poll rate')}: ${1000 / POLL_MSEC} Hz`);
--- src/index.ts
+++ src/index.ts
@@ -102,7 +102,7 @@ function colorsForPanel(connections: Array<Connection>, panel: Panel | null): Ar
     console.log('Button event on P%d (button currently %s)', pin, state);
   }
 
-  rpio.poll(8, pollcb, rpio.PULL_DOWN);
+  rpio.poll(8, pollcb, rpio.POLL_LOW);
   // rpio.poll(10, pollcb);
 
   console.log('Ganglia Daemon is reborn!\n')
